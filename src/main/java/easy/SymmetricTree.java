191601483972d045c47ac5024a733bfbe88f6d00
rudniev.viacheslav@gmail.com <os4C7PAYU3fn>
2021-02-21 20:40:04 +0100

    add 2 Trees sampels

---
+++ src/main/java/easy/SymmetricTree.java
@@ -0,0 +1,46 @@
+package easy;
+
+/**
+ * https://leetcode.com/problems/symmetric-tree/
+ *
+ * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,2,2,3,4,4,3]
+ * Output: true
+ * Example 2:
+ *
+ *
+ * Input: root = [1,2,2,null,3,null,3]
+ * Output: false
+ */
+
+public class SymmetricTree {
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode() {}
+		TreeNode(int val) { this.val = val; }
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public boolean isSymmetric(TreeNode root) {
+		return isMirror(root.left, root.right);
+	}
+
+	public boolean isMirror(TreeNode t1, TreeNode t2) {
+		if (t1 == null && t2 == null) return true;
+		if (t1 == null || t2 == null) return false;
+		return (t1.val == t2.val) && isMirror(t1.right, t2.left) && isMirror(t1.left, t2.right);
+	}
+
+
+}
